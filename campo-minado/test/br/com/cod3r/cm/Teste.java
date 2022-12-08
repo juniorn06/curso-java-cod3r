b23b027088ad72ad045590be310661376bf2bef8
Junior <juniorn06@gmail.com>
2022-12-08 16:51:46 -0300

    Configurar JUnit

---
+++ campo-minado/test/br/com/cod3r/cm/Teste.java
@@ -0,0 +1,25 @@
+package br.com.cod3r.cm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class Teste {
+
+    @Test
+    void testarSeIgualADois(){
+
+        int a = 1 + 1;
+
+        assertEquals(2, a);
+    }
+
+    @Test
+    void testarSeIgualATres(){
+
+        int x = 2 + 10 - 9;
+
+        assertEquals(3, x);
+    }
+}
