add6cb53b66bced08c948bc7adb3796a1cc9b061
Junior <juniorn06@gmail.com>
2022-12-19 17:36:52 -0300

    Classe Tabuleiro Console

--- campo-minado/src/br/com/cod3r/cm/Aplicacao.java
+++ campo-minado/src/br/com/cod3r/cm/Aplicacao.java
@@ -1,17 +1,13 @@
 package br.com.cod3r.cm;
 
 import br.com.cod3r.cm.modelo.Tabuleiro;
+import br.com.cod3r.cm.visao.TabuleiroConsole;
 
-public class aplicacao {
+public class Aplicacao {
 
     public static void main(String[] args) {
 
         Tabuleiro tabuleiro = new Tabuleiro(7,7,7);
-
-        tabuleiro.alterarMarcacao(4,4);
-        tabuleiro.alterarMarcacao(4,5);
-        tabuleiro.abir(3,3);
-        System.out.println(tabuleiro);
-
+        new TabuleiroConsole(tabuleiro);
     }
 }
