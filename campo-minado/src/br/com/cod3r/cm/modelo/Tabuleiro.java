ee9d15ebe9a833317bb1b7a6b8b9960bb0aea019
Junior <juniorn06@gmail.com>
2022-12-19 16:09:33 -0300

    Classe Tabuleiro #03

--- campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -22,6 +22,14 @@ public class Tabuleiro {
         sortearMinas();
     }
 
+    public void abir(int linha, int coluna){
+        campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna).findFirst().ifPresent(Campo::alternarMarcacao);
+    }
+
+    public void alterarMarcacao(int linha, int coluna){
+        campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna).findFirst().ifPresent(Campo::abrir);
+    }
+
     private void gerarCampos() {
         for (int linha = 0; linha < linhas; linha++) {
             for (int coluna = 0; coluna < colunas; coluna++) {
@@ -57,5 +65,22 @@ public class Tabuleiro {
     public void reiniciar(){
         campos.stream().forEach(Campo::reiniciar);
         sortearMinas();
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        for (int l = 0; l < linhas; l++) {
+            for (int c = 0; c < colunas; c++) {
+                sb.append(" ");
+                sb.append(campos.get(i));
+                sb.append(" ");
+                i++;
+            }
+            sb.append("\n");
+        }
+        
+        return sb.toString();
     }
 }
