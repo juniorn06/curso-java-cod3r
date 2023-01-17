a713ff3bcdb5a8d3aa986fcf4cc847e35aa2b999
Junior <juniorn06@gmail.com>
2023-01-17 16:55:29 -0300

    Alterar Usuário #01

---
+++ curso/exercicios-jpa/src/main/java/teste/basico/AlterarUsuario1.java
@@ -0,0 +1,29 @@
+package teste.basico;
+
+import modelo.basico.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class AlterarUsuario1 {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Usuario usuario = em.find(Usuario.class, 6L);
+        usuario.setNome("Aline");
+        usuario.setEmail("aline@gmail.com");
+
+        em.merge(usuario);
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+}
